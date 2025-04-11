d988f54dedaf1f207795c81b0dcab3b676983f67
Jiwon Park <parkjiwon0821@naver.com>
2025-04-11 20:04:25 +0900

    투두리스트 실습

---
+++ work4/script2.js
@@ -0,0 +1,26 @@
+const toDoForm = document.querySelector("#toDoForm");
+const toDoInput = document.querySelector("#toDoInput");
+const toDoList = document.querySelector("#toDoList");
+
+toDoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const toDoText = toDoInput.value.trim();
+  if (toDoText === "") return;
+
+  const toDoItem = document.createElement("li");
+  toDoItem.innerText = toDoText;
+  toDoItem.className = "toDoItem";
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "삭제";
+  deleteBtn.className = "deleteBtn";
+
+  deleteBtn.addEventListener("click", () => {
+    toDoList.removeChild(toDoItem);
+  });
+
+  toDoItem.appendChild(deleteBtn);
+  toDoList.appendChild(toDoItem);
+  toDoInput.value = "";
+})
