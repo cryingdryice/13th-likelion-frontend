f42b3dcfde7ae498cd3610a7262ea2bccaa320da
Jiwon Park <parkjiwon0821@naver.com>
2025-04-11 19:48:43 +0900

    동적페이지 실습

--- work4/script.js
+++ work4/script.js
@@ -87,3 +87,42 @@
 // btn.addEventListener("click", handleClick2);
 
 // btn.removeEventListener("click", handleClick1);
+
+let count = 0;
+
+const countBtn = document.querySelector("#countBtn");
+const resetBtn = document.querySelector("#resetBtn");
+
+countBtn.innerText = count;
+
+countBtn.addEventListener("click", () => {
+    countBtn.innerText = ++count;
+});
+
+resetBtn.addEventListener("click", () => {
+    count = 0;
+    countBtn.innerText = count;
+});
+
+const colorSelect = document.querySelector("#colorSelect");
+const colorBox = document.querySelector("#colorBox");
+
+colorBox.style.width = "100px";
+colorBox.style.height = "100px";
+colorBox.style.backgroundColor = "gray";
+colorBox.style.marginTop = "10px";
+
+colorSelect.addEventListener("change", (e) => {
+    console.log(e);
+    colorBox.style.backgroundColor = e.target.value;
+});
+
+const nameForm = document.querySelector("#nameForm");
+const nameInput = document.querySelector("#nameInput");
+const submitResult = document.querySelector("#submitResult");
+
+nameForm.addEventListener("submit", (e) => {
+    console.log(e);
+    e.preventDefault();
+    submitResult.innerText = `안녕하세요. ${nameInput.value}님!`;
+});
