fa1e9a1a5dc1ed52cdd6b3d3a1f3c390eb8647e2
Jiwon Park <parkjiwon0821@naver.com>
2025-05-09 20:00:35 +0900

    feat: 타이머 구현

--- work8/src/App.jsx
+++ work8/src/App.jsx
@@ -1,13 +1,15 @@
 import HookStructure from "./HookStructure";
 import ItemList from "./ItemList";
 import NameInput from "./NameInput";
+import TimerSwitch from "./TimerSwitch";
 
 const App = () => {
 	return (
 		<div>
 			{/* <NameInput /> */}
 			{/* <HookStructure /> */}
-			<ItemList />
+			{/* <ItemList /> */}
+			<TimerSwitch />
 		</div>
 	);
 };
